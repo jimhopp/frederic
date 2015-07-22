a95b993f9efe6bfe754d676e9b1d4bb25a28055d
Jim Hopp <jimhopp@gmail.com>
2015-07-22 14:05:16 -0700

    Test for addclient, plus clean-up and fixes   - add test for /api/addclient   - fix incorrect error message   - remove unused func

--- login_test.go
+++ login_test.go
@@ -5,17 +5,11 @@ import (
     "net/http"
     "net/http/httptest"
     "bytes"
-    "fmt"
 
     "appengine"
     "appengine/user"
     "appengine/aetest"
 )
-
-func addclientx(c appengine.Context, w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusCreated)
-    fmt.Fprintln(w, "client added")
-}
 
 func TestHomePageNotLoggedIn(t *testing.T) {
     inst, err := aetest.NewInstance(nil)
@@ -60,11 +54,42 @@ func TestHomePageLoggedIn(t *testing.T) {
 
     code := w.Code
     if code != http.StatusOK {
-        t.Errorf("got code %v, want %v", code, http.StatusFound)
+        t.Errorf("got code %v, want %v", code, http.StatusOK)
     }
 
     body := w.Body.Bytes()
     if !bytes.Equal(body, []byte("This is the SVdP Clients homepage.\n\nYou are authenticated as test@example.org")) {
         t.Errorf("got body %v, want %v", body, []byte("This is the SVdP Clients homepage.\n\nYou are authenticated as test@example.org"))
+    }
+}
+
+
+func TestAddClient(t *testing.T) {
+    inst, err := aetest.NewInstance(nil)
+    if err != nil {
+            t.Fatalf("Failed to create instance: %v", err)
+    }
+    defer inst.Close()
+
+    req, err := inst.NewRequest("GET", "/api/addclient", nil)
+    if err != nil {
+            t.Fatalf("Failed to create req1: %v", err)
+    }
+
+    aetest.Login(&user.User{Email: "test@example.org"}, req)
+
+    w := httptest.NewRecorder()
+    c := appengine.NewContext(req)
+
+    addclient(c, w, req)
+
+    code := w.Code
+    if code != http.StatusCreated {
+        t.Errorf("got code %v, want %v", code, http.StatusCreated)
+    }
+
+    body := w.Body.Bytes()
+    if !bytes.Equal(body, []byte("client added\n")) {
+        t.Errorf("got body %v, want %v", body, []byte("client added\n"))
     }
 }

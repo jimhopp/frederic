b6b33d18abcfe1dc02f59f74916df48317e9a208
Jim Hopp <jimhopp@gmail.com>
2016-01-25 19:00:49 -0800

    Show date fmt label for browsers that don't support input dates. Fixes #27. * Also fixes some minor html errors

--- js/json.js
+++ js/json.js
@@ -1,3 +1,12 @@
-function jsonMe(obj) {
-    return JSON.stringify({type:"Fiat", model:500, color:"white"}, nil);
+function browserSupportsDateInput() {
+  var b = false; 
+  try {
+    var tester = document.createElement('input');
+    tester.type = "date";
+    console.log("tester.type=" + tester.type);
+    console.log("browser supports date input?" + (tester.type === "date"));
+    b = tester.type === "date"; 
+  } catch (e) {
+    console.log("oops, got error testing browser date input support" + e);
+  } finally {return b;}
 }
